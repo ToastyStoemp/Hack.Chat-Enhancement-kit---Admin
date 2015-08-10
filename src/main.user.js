599dc4c19af9e4f8c891d8d8aa7501b3a3483ae7
Wolf Van Herreweghe <foreverwolf@live.be>
2015-08-10 15:06:56 +0200

    updates and shit

--- src/main.user.js
+++ src/main.user.js
@@ -1,3 +1,8 @@
+var messageSound = new Audio('https://www.freesound.org/people/bubaproducer/sounds/107156/download/107156__bubaproducer__button-9-funny.wav');
+var notifySound = new Audio('https://www.freesound.org/people/JustinBW/sounds/80921/download/80921__justinbw__buttonchime02up.wav');
+var modSound = new Audio('https://www.freesound.org/people/JustinBW/sounds/80921/download/80921__justinbw__buttonchime02up.wav');
+var canCallMod = true;
+
 var sidebar = document.getElementById("sidebar-content");
 
 var para = document.createElement("p");
@@ -9,6 +14,26 @@ para.appendChild(NotCheckbox);
 para.appendChild(text);
 sidebar.appendChild(para);
 sidebar.insertBefore(para, sidebar.childNodes[4]);
+
+para = document.createElement("p");
+var SoundCheckbox = document.createElement("INPUT");
+SoundCheckbox.type = "checkbox";
+SoundCheckbox.checked = true;
+text = document.createTextNode("Sound");
+para.appendChild(SoundCheckbox);
+para.appendChild(text);
+sidebar.appendChild(para);
+sidebar.insertBefore(para, sidebar.childNodes[5]);
+
+para = document.createElement("p");
+var AlarmCheckbox = document.createElement("INPUT");
+AlarmCheckbox.type = "checkbox";
+AlarmCheckbox.checked = true;
+text = document.createTextNode("Alarm me");
+para.appendChild(AlarmCheckbox);
+para.appendChild(text);
+sidebar.appendChild(para);
+sidebar.insertBefore(para, sidebar.childNodes[6]);
 
 para = document.createElement("p");
 var btn = document.createElement("BUTTON");
@@ -23,7 +48,7 @@ btn.onclick = function() {
 };
 para.appendChild(btn);
 sidebar.appendChild(para);
-sidebar.insertBefore(para, sidebar.childNodes[5]);
+sidebar.insertBefore(para, sidebar.childNodes[7]);
 
 para = document.createElement("p");
 btn = document.createElement("BUTTON");
@@ -36,7 +61,7 @@ btn.onclick = function() {
 };
 para.appendChild(btn);
 sidebar.appendChild(para);
-sidebar.insertBefore(para, sidebar.childNodes[6]);
+sidebar.insertBefore(para, sidebar.childNodes[8]);
 
 if (pushMessageOrig)
   pushMessage = pushMessageOrig;
@@ -45,11 +70,33 @@ var pushMessageOrig = pushMessage;
 var yourNick = myNick.split("#")[0];
 pushMessage = function(args) {
   pushMessageOrig(args);
+  var msg = args.text;
+  if (msg.indexOf("invited you to ?") != -1) {
+    var nick = msg.substr(0, msg.indexOf(' '));
+    var channel = msg.substr(msg.indexOf('?') + 1, 8);
+    notifyMe(nick + " invited you", "Click here to accept.", false, channel);
+  }
   if (args.nick != "*") {
-    var msg = args.text;
-    if (msg.indexOf(yourNick) != -1 && !document.hasFocus())
+    if (msg.indexOf('.callMod') != -1) {
+      if (canCallMod) {
+        send({
+          cmd: 'chat',
+          nick: myNick,
+          text: '@' + args.nick + ' I have been alarmed'
+        });
+        var begin = msg.indexOf(' ') + 1;
+        var end = msg.indexOf(' ', begin);
+        var suspectlength = end - begin;
+        var suspect = msg.substr(begin, suspectlength);
+        _callMod(args.nick, suspect, msg.substr(end, msg.length - end));
+      }
+    }
+    if (msg.indexOf(yourNick) != -1 && !document.hasFocus()) {
       if (NotCheckbox.checked)
-        notifyMe(args.nick, args.text);
+        notifyMe(args.nick + " mentioned you", args.text, false);
+    } else
+    if (SoundCheckbox.checked)
+      messageSound.play();
   }
   if (document.hasFocus()) {
     window.unread = 0;
@@ -68,7 +115,7 @@ window.onfocus = function() {
   window.updateTitle();
 }
 
-function notifyMe(sender, text) {
+function notifyMe(title, text, channel) {
   if (!Notification) {
     alert('Desktop notifications not available in your browser. Try Chrome.');
     return;
@@ -77,35 +124,70 @@ function notifyMe(sender, text) {
   if (Notification.permission !== "granted")
     Notification.requestPermission();
   else
-    _notifiyMe(sender, text);
+    _notifiyMe(title, text, channel);
 }
 
-function _notifiyMe(sender, text) {
-  var not = new Notification(sender + " mentioned you", {
+function _notifiyMe(title, text, channel) {
+  if (SoundCheckbox.checked)
+    notifySound.play();
+  var Channel = channel;
+  var not = new Notification(title, {
     body: text,
-    icon : 'http://i.imgur.com/44B3G6a.png'
+    icon: 'http://i.imgur.com/44B3G6a.png'
   });
 
+
   not.onclick = function() {
-    window.focus()
+    if (Channel) {
+      console.log(Channel);
+      window.open('https://hack.chat/?' + Channel, '_blank');
+    } else
+      window.focus()
   };
   setTimeout(function() {
     not.close();
     notifications.splice(notifications.indexOf(not), 1);
-  }, 4000);
+  }, 8000);
   notifications.push(not);
 }
 
 
-/*
-$\color{orange}{\large{Hack.Chat \space chrome \space extension}} \space \color{lightblue}{0.0.3}$
-download here: https://db.tt/8ErFlCcq
-just drag and drop in: chrome://extensions/
+function _callMod(sender, suspect, reason) {
+  if (canCallMod && AlarmCheckbox.checked) {
+    canCallMod = false;
+    modSound.play();
+    var not = new Notification(sender + ' requested a moderator', {
+      body: suspect + " is under suspicion of " + reason,
+      icon: 'http://i.imgur.com/44B3G6a.png'
+    });
+    not.onclick = function() {
+      window.focus()
+    };
+    setTimeout(function() {
+      canCallMod = true;
+    }, 30000);
+    setTimeout(function() {
+      not.close();
+      notifications.splice(notifications.indexOf(not), 1);
+    }, 8000);
+  }
+}
 
-Fixed;
-- unread message count does no longer count while tab is active
-- final version of the notification system, this time everything should work 100%
 
-Added;
-- Icon for notifications
-*/
+// $\color{orange}{\large{Hack.Chat \space chrome \space extension}} \space \color{lightblue}{0.0.4}$
+// $made \space by \space \color{cyan}{ToastyStoemp}$
+// download here: https://db.tt/8ErFlCcq
+// just drag and drop in: chrome://extensions/
+// GitHub: http://bit.ly/1ISXn6b
+//
+// Fixed:
+// - Fixed bug with the admin version notification not working every now and then
+// - Doubled the length of the notifications
+//
+// Added:
+// - sound for chat, and notifications
+// - option for sound
+// - Call mod button
+// - Special notification receiver for mods only admin version
+//
+// More to come!
